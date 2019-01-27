83d31ddb8bf5b492d3bcd5cc24136e670b76bde5
Mihaly Farkas <mihaly@farkas.co>
2019-01-27 15:49:49 +0100

    Project generated

---
+++ README.md
@@ -0,0 +1,17 @@
+[![Build Status][travis-ci-shield-img]][travis-ci-shield-link]
+[![Latest Stable][bintray-shield-img]][bintray-shield-link]
+[![Quality Gate][sonarcloud-shield-img]][sonarcloud-shield-link]
+[![Site Docs][github-pages-shield-img]][github-pages-shield-link]
+
+# Turbo Giggle
+
+Yet another Turbo Giggle application.
+
+[travis-ci-shield-img]:     https://img.shields.io/travis/34w5dtfgs/34w5dtfgs-turbo-giggle.svg
+[travis-ci-shield-link]:    https://travis-ci.org/34w5dtfgs-turbo-giggle
+[bintray-shield-img]:       https://api.bintray.com/packages/34w5dtfgs/maven/34w5dtfgs-turbo-giggle/images/download.svg
+[bintray-shield-link]:      https://bintray.com/34w5dtfgs/maven/34w5dtfgs-turbo-giggle/_latestVersion
+[sonarcloud-shield-img]:    https://sonarcloud.io/api/project_badges/measure?project=34w5dtfgs.application%3A34w5dtfgs-turbo-giggle&metric=alert_status
+[sonarcloud-shield-link]:   https://sonarcloud.io/dashboard?id=34w5dtfgs.application%3A34w5dtfgs-turbo-giggle
+[github-pages-shield-img]:  https://img.shields.io/badge/dynamic/json.svg?label=Docs&colorB=0f80c0&query=$.version&uri=https://34w5dtfgs.github.io/34w5dtfgs-turbo-giggle/artifact-info.json
+[github-pages-shield-link]: https://34w5dtfgs.github.io/34w5dtfgs-turbo-giggle/
---
+++ src/test/java/com/34w5dtfgs/turbo_giggle/AppTest.java
@@ -0,0 +1,54 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2019 34w5dtfgs, the Turboest Giggler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.34w5dtfgs.turbo_giggle;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+  private ByteArrayOutputStream outputStream;
+
+  @BeforeTest
+  public void setUpStreams() {
+    outputStream = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outputStream));
+  }
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    App.main();
+    String output = outputStream.toString();
+    assertThat(output, equalTo("Hello World!" + System.lineSeparator()));
+  }
+
+  @AfterTest
+  public void restoreStreams() {
+    System.setOut(System.out);
+  }
+}
